7af80a5d8d89fb23a4db05b28c0af986276c5e98
Juan Cruz Rodriguez <jcrodriguez@bdmg.com.ar>
2019-05-29 08:47:48 -0300

    New optimizer: Constant Folding

--- R/parse.R
+++ R/parse.R
@@ -2,9 +2,46 @@
 #
 # @param text Text to parse.
 #
-parse_flat_data <- function(text) {
-  parsed_text <- base::parse(text = text)
-  utils::getParseData(parsed_text)
+parse_flat_data <- function(text, include_text = NA) {
+  parsed_text <- base::parse(text = text, keep.source = TRUE)
+  pd <- utils::getParseData(parsed_text, includeText = include_text)
+
+  # pos_id to reorder code text
+  pd$pos_id <- seq(1L, nrow(pd))
+
+  # next_spaces and lines after each text
+  pd$next_spaces[pd$terminal] <- 0
+  pd$next_lines[pd$terminal] <- 0
+  pd$prev_spaces[pd$terminal] <- 0
+
+  pd_terms <- pd[pd$terminal,]
+
+  # next_spaces will be the difference between the column next text starts, and
+  # the column this text ends
+  pd_terms$next_spaces[-nrow(pd_terms)] <-
+    pd_terms$col1[-1] - pd_terms$col2[-nrow(pd_terms)] -1
+  pd[pd$terminal, "next_spaces"] <- pmax(0, pd_terms$next_spaces)
+
+  # next_lines will be the difference between the line next text starts, and
+  # the line this text ends
+  pd_terms$next_lines[-nrow(pd_terms)] <-
+    pd_terms$line1[-1] - pd_terms$line2[-nrow(pd_terms)]
+  pd[pd$terminal, "next_lines"] <- pmax(0, pd_terms$next_lines)
+
+  # prev_spaces will be the column where each text starts, if the previous text
+  # had a new line
+  pd_terms$prev_spaces[which(pd_terms$next_lines > 0) +1] <-
+    pd_terms[which(pd_terms$next_lines > 0) +1, "col1"] -1
+  pd[pd$terminal, "prev_spaces"] <- pd_terms$prev_spaces
+
+  # pd$token_before <- NA
+  # pd$token_after <- NA
+
+  # terminals <- pd[pd$terminal,]
+  # terminals$token_after <- c(terminals$token[seq(2, nrow(terminals))], NA)
+  # terminals$token_before <- c(NA, terminals$token[seq(1, nrow(terminals)-1)])
+  # pd[pd$terminal,] <- terminals
+  return(pd)
 }
 
 # Deparses a flat parsed data data.frame to text
@@ -14,23 +51,30 @@ parse_flat_data <- function(text) {
 deparse_flat_data <- function(fpd) {
   fpd_terms <- fpd[fpd$terminal, ]
 
-  old_line <- 1
-  old_col <- 0
   res <- ""
   for (i in seq_len(nrow(fpd_terms))) {
     act_pd <- fpd_terms[i, ]
-    act_line <- act_pd$line1
-    new_lines <- act_line - old_line
-    if (new_lines > 0) {
-      old_col <- 0
-      res <- paste0(res, paste0(rep("\n", new_lines), collapse = ""))
-    }
-    act_col <- act_pd$col1
-    res <- paste0(res, paste0(rep(" ", max(0, act_col - old_col - 1)), collapse = ""))
-    res <- paste0(res, act_pd$text)
-    old_line <- act_line
-    old_col <- act_pd$col2
+    res <- paste0(res, paste0(
+      paste0(rep(" ", act_pd$prev_spaces), collapse = ""),
+      act_pd$text,
+      paste0(rep(" ", act_pd$next_spaces), collapse = ""),
+      paste0(rep("\n", act_pd$next_lines), collapse = "")
+    ))
   }
   return(res)
 }
 
+# Gets a sub fpd with all the children from a node ID
+#
+# @param fpd a flat parsed data data.frame to deparse.
+# @param id Numeric indicating the parent ID.
+#
+get_children <- function(fpd, id) {
+  act_fpd <- fpd[fpd$id == id,]
+  act_childs <- fpd[fpd$parent == id, "id"]
+  while (length(act_childs) > 0) {
+    act_fpd <- rbind(act_fpd, fpd[fpd$id %in% act_childs,])
+    act_childs <- fpd[fpd$parent %in% fpd[fpd$id %in% act_childs, "id"], "id"]
+  }
+  return(act_fpd)
+}
---
+++ tests/testthat/test-opt_constant_folding.R
@@ -0,0 +1,178 @@
+context("opt_constant_folding")
+
+test_that("correctly constant fold", {
+  code <- paste(
+    "x <- - 1 + 2 - 3 * 4 / 5 ^ 6",
+    "x <- (1000 + 2 - 3 * 4) / 5 ^ (6 - 1)",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 >  3 * 1",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 >= 3",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 <  3",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 <=  3",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 == 3",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 != 3",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 != 3 & TRUE",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 != 3 & FALSE",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 != 3 | TRUE",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 != 3 | FALSE",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 != 3 && TRUE",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 != 3 && FALSE",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 != 3 || TRUE",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 != 3 || FALSE",
+    "y <- 14 + 14 + x + 14 + 14",
+    "for (i in 1:100) {",
+    "  i <- i + 7 * 3",
+    "}",
+    sep = "\n")
+  opt_code <- opt_constant_folding(list(code))$codes[[1]]
+  expect_equal(opt_code, paste(
+    "x <- 0.999232",
+    "x <- 0.3168",
+    "x <- 0.999232",
+    "x <- FALSE",
+    "x <- FALSE",
+    "x <- TRUE",
+    "x <- TRUE",
+    "x <- FALSE",
+    "x <- TRUE",
+    "x <- TRUE",
+    "x <- FALSE",
+    "x <- TRUE",
+    "x <- TRUE",
+    "x <- TRUE",
+    "x <- FALSE",
+    "x <- TRUE",
+    "x <- TRUE",
+    "y <- 28 + x + 14 + 14",
+    "for (i in 1:100) {",
+    "  i <- i + 21",
+    "}",
+    sep = "\n"
+  ))
+})
+
+test_that("dont fold floats", {
+  code <- paste(
+    "x <- - 1 + 2 - 3 * 4 / 5 ^ 6",
+    "x <- (1000 + 2 - 3 * 4) / 5 ^ (6 - 1)",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 >  3 * 1",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 >= 3",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 <  3",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 <=  3",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 == 3",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 != 3",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 != 3 & TRUE",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 != 3 & FALSE",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 != 3 | TRUE",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 != 3 | FALSE",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 != 3 && TRUE",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 != 3 && FALSE",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 != 3 || TRUE",
+    "x <- 2 - 3 * 4 / 5 ^ 6 - 1 != 3 || FALSE",
+    "y <- 14 + 14 + x + 14 + 14",
+    "for (i in 1:100) {",
+    "  i <- i + 7 * 3",
+    "}",
+    sep = "\n")
+  opt_code <- opt_constant_folding(list(code), fold_floats = FALSE)$codes[[1]]
+  expect_equal(opt_code, paste(
+    "x <- 1 - 12 / 15625",
+    "x <- 990 / 3125",
+    "x <- 2 - 12 / 15625 - 1",
+    "x <- FALSE",
+    "x <- FALSE",
+    "x <- TRUE",
+    "x <- TRUE",
+    "x <- FALSE",
+    "x <- TRUE",
+    "x <- TRUE",
+    "x <- FALSE",
+    "x <- TRUE",
+    "x <- TRUE",
+    "x <- TRUE",
+    "x <- FALSE",
+    "x <- TRUE",
+    "x <- TRUE",
+    "y <- 28 + x + 14 + 14",
+    "for (i in 1:100) {",
+    "  i <- i + 21",
+    "}",
+    sep = "\n"
+  ))
+})
+
+test_that("constant fold in while", {
+  code <- paste(
+    "i <- 0 * 512;",
+    "n <- 100",
+    "res <- 0",
+    "while (i <n) {",
+    "  (TRUE || 0 / 2 == 0)",
+    "  if (TRUE || 0 / 2 == 0)",
+    "    i <- (0 + (1^1)) * (i / 1)",
+    "  if (0+i %% 2 == 0)",
+    "    res <- res + 1",
+    "  i <- i + 1",
+    "}",
+    sep = "\n")
+  opt_code <- opt_constant_folding(list(code))$codes[[1]]
+  expect_equal(opt_code, paste(
+    "i <- 0;",
+    "n <- 100",
+    "res <- 0",
+    "while (i <n) {",
+    "  TRUE",
+    "  if (TRUE)",
+    "    i <- 1 * (i / 1)",
+    "  if (0+i %% 2 == 0)",
+    "    res <- res + 1",
+    "  i <- i + 1",
+    "}",
+    sep = "\n"
+  ))
+
+  env1 <- new.env()
+  eval(parse(text = code), envir = env1)
+  env2 <- new.env()
+  eval(parse(text = opt_code), envir = env2)
+
+  expect_equal(as.list(env1), as.list(env2))
+})
+
+test_that("constant fold in function", {
+  code <- paste(
+    "foo <- function() {",
+    "  return(2 - 3 * 4 / 5 ^ 6 - 1 != 3 || FALSE)",
+    "}",
+    "",
+    "bar <- function(x) {",
+    "  return(2 - 3 * 4 / 5 ^ 6 - 1 != 3 && x)",
+    "}",
+    "",
+    "res <- bar(TRUE)",
+    sep = "\n")
+  opt_code <- opt_constant_folding(list(code))$codes[[1]]
+  expect_equal(opt_code, paste(
+    "foo <- function() {",
+    "  return(TRUE)",
+    "}",
+    "",
+    "bar <- function(x) {",
+    "  return(TRUE && x)",
+    "}",
+    "",
+    "res <- bar(TRUE)",
+    sep = "\n"
+  ))
+
+  env1 <- new.env()
+  eval(parse(text = code), envir = env1)
+  env2 <- new.env()
+  eval(parse(text = opt_code), envir = env2)
+  env1 <- as.list(env1)
+  env2 <- as.list(env2)
+
+  expect_equal(names(env1), names(env1))
+  expect_equal(env1$res, env2$res)
+})
