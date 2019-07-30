6074df57df3af7989166d75c4548064ef8d3416a
Juan Cruz Rodriguez <jcrodriguez@bdmg.com.ar>
2019-07-30 12:14:09 -0300

    Improving Shiny GUIs

--- inst/shiny/pkg_optimizer/server.R
+++ inst/shiny/pkg_optimizer/server.R
@@ -9,9 +9,23 @@ shinyServer(function(input, output, session) {
   ############ Package loader panel
 
   observeEvent(input$optimize, {
+    showModal(modalDialog(
+      "Meanwhile, enjoy this random gif:", br(), br(),
+      HTML(.random_gif()),
+      title = "Optimization in progress ...",
+      size = "m",
+      footer = NULL,
+      easyClose = FALSE
+    ))
+    progress <- Progress$new(session, min=1, max=4)
+    on.exit(progress$close())
+    progress$set(message = "Downloading package", value = 2,
+                 detail = "This may take a while...")
+
     pkg_dir <- download_pkg(input$pkg)
     if (inherits(pkg_dir, "try-error")) {
       showNotification("Could not download package", type = "error")
+      removeModal()
       return(NULL)
     }
 
@@ -23,6 +37,8 @@ shinyServer(function(input, output, session) {
     opt_files <- dir(paste0(opt_pkg_dir, "/R"), full.names = TRUE)
 
     sel_optimizers <- rco::all_optimizers[input$opt_list]
+    progress$set(message = "Optimizing package", value = 3,
+                 detail = "This may take a while...")
     opt_res <- rco::optimize_files(opt_files,
       overwrite = TRUE,
       optimizers = sel_optimizers
@@ -36,6 +52,7 @@ shinyServer(function(input, output, session) {
       pkg_dir = pkg_dir, opt_pkg_dir = opt_pkg_dir,
       opt_files = opt_files
     ))
+    removeModal()
   })
 
   ############ Optimized code panel
