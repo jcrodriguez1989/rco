736b34a5b1531615977df2600a04fee4f6e65573
Juan Cruz Rodriguez <jcrodriguez@bdmg.com.ar>
2019-07-30 10:38:38 -0300

    Centering giphy img

--- inst/shiny/code_optimizer/global.R
+++ inst/shiny/code_optimizer/global.R
@@ -4,17 +4,18 @@
   search_raw <- try({
     httr::GET(
       url = "http://api.giphy.com/",
-      path = "/v1/gifs/random",
+      path = "/v1/gifs/trending",
       query = list(api_key = "GAUeYalixQovJJGRACGEaKRpNunOoH1q")
     )
   })
 
   res <- ""
   if (!inherits(search_raw, "try-error") && search_raw$status_code == 200) {
+    img <- sample(httr::content(search_raw)$data, 1)[[1]]
     res <- paste0(
-      '<img src="',
-      httr::content(search_raw)$data$images$fixed_height$url,
-      '">'
+      '<center><img src="',
+      img$images$fixed_height$url,
+      '"></center>'
     )
   }
   res
