5e64d312893417ba0ca43f8b7185a995dfd23c3b
Umang Majumder <54509868+ZeroDawn0D@users.noreply.github.com>
2019-12-13 14:44:36 +0530

    Update opt-dead-store.R

--- R/opt-dead-store.R
+++ R/opt-dead-store.R
@@ -14,6 +14,17 @@
 #'   sep = "\n"
 #' )
 #' cat(opt_dead_store(list(code))$codes[[1]])
+#' code1 <- paste(
+#'     "sinpi <- function() {",
+#'     "pi <- 3.1415",
+#'     "e <- 2.718",
+#'     "phi <- 1.618",
+#'     "sin(pi)",
+#'     "}",
+#'     sep = "\n"
+#'   )
+#'cat(opt_dead_store(list(code1))$codes[[1]])
+
 #' @export
 #'
 opt_dead_store <- function(texts) {
