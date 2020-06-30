0ac46fd5a7bff567da0854ad5f16c72023f5e0c5
Rahul Saxena <rahulsaxena.pro@gmail.com>
2020-07-01 01:31:39 +0530

    Removing spacing issues

--- R/opt-loop-invariant.R
+++ R/opt-loop-invariant.R
@@ -40,11 +40,11 @@ li_one_file <- function(text) {
   new_pd <- pd[pd$parent >= 0, ] # keep lines with just comments
   new_pd <- li_one_pd(new_pd)
   res_pd <- rbind(res_pd, new_pd)
-  
+
   if (nrow(res_pd) > 0) {
     res_pd <- res_pd[order(res_pd$pos_id), ]
   }
-  
+
   deparse_data(res_pd)
 }
 
@@ -54,26 +54,26 @@ li_one_file <- function(text) {
 #
 li_one_pd <- function(pd) {
   res_pd <- pd
-  
+
   # get loops
   # For the moment, remove `repeat` loop invariant
   loop_parent_ids <- pd$parent[pd$token %in% c("FOR", "WHILE")] # %in% loops]
-  
+
   # remove loops that have function calls inside
   loop_parent_ids <- loop_parent_ids[!sapply(loop_parent_ids, function(act_prnt) {
     "SYMBOL_FUNCTION_CALL" %in% get_children(pd, act_prnt)$token
   })]
-  
+
   # remove loops that have next or break calls inside
   loop_parent_ids <- loop_parent_ids[!sapply(loop_parent_ids, function(act_prnt) {
     any(c("BREAK", "NEXT") %in% get_children(pd, act_prnt)$token)
   })]
-  
+
   # for each loop do the invariant code motion
   for (loop_parent_id in loop_parent_ids) {
     res_pd <- li_in_loop(res_pd, loop_parent_id)
   }
-  
+
   res_pd
 }
 
@@ -84,7 +84,7 @@ li_one_pd <- function(pd) {
 #
 li_in_loop <- function(pd, id) {
   lv_vars <- get_loop_variant_vars(pd, id)
-  
+
   # start visiting the loop body
   visit_nodes <- utils::tail(pd$id[pd$parent == id], 1)
   to_unloop_ids <- c()
@@ -97,7 +97,7 @@ li_in_loop <- function(pd, id) {
         new_visit <- c(new_visit, act_sblngs$id[!act_sblngs$terminal])
       } else if (all(
         act_pd$token %in%
-        c(ops, precedence_ops, constants, assigns, "expr", "SYMBOL")
+          c(ops, precedence_ops, constants, assigns, "expr", "SYMBOL")
       )) {
         if (!any(lv_vars %in% act_pd$text[act_pd$token == "SYMBOL"])) {
           to_unloop_ids <- c(to_unloop_ids, act_parent)
@@ -106,7 +106,7 @@ li_in_loop <- function(pd, id) {
     }
     visit_nodes <- new_visit
   }
-  
+
   unloop_expr(pd, to_unloop_ids, id)
 }
 
@@ -120,13 +120,13 @@ unloop_expr <- function(pd, exprs_ids, loop_id) {
   if (length(exprs_ids) == 0) {
     return(pd)
   }
-  
+
   res_pd <- remove_nodes(pd, exprs_ids)
   loop_pd <- get_children(pd, loop_id)
   exprs_pd <- get_children(loop_pd, exprs_ids)
   exprs <- deparse_data(exprs_pd)
   exprs <- sub("^\n*", "", exprs)
-  
+
   loop_token <- pd$token[pd$parent == loop_id][[1]]
   if (loop_token == "WHILE") {
     loop_cond_id <- pd$id[pd$parent == loop_id & pd$token == "expr"][[1]]
@@ -144,7 +144,7 @@ unloop_expr <- function(pd, exprs_ids, loop_id) {
     )
     new_expr <- paste0("if (length(", loop_cond, ") > 0) {\n", exprs, "}")
   }
-  
+
   new_expr_pd <- parse_text(new_expr)
   new_expr_pd$prev_spaces[new_expr_pd$terminal][[1]] <-
     loop_pd$prev_spaces[loop_pd$terminal][[1]]
@@ -153,13 +153,13 @@ unloop_expr <- function(pd, exprs_ids, loop_id) {
   new_expr_pd$prev_spaces[nrow(new_expr_pd)] <-
     loop_pd$prev_spaces[loop_pd$terminal][[1]]
   new_expr_pd$next_lines[nrow(new_expr_pd)] <- 1
-  
-  
+
+
   if (any(exprs_ids %in% utils::tail(pd$id[pd$parent == loop_id]))) {
     # if we are removing the entire loop body, then remove the loop
     res_pd <- remove_nodes(res_pd, loop_id)
   }
-  
+
   rbind(
     res_pd,
     replace_pd(get_children(loop_pd, loop_id), new_expr_pd)
@@ -175,21 +175,21 @@ get_loop_variant_vars <- function(pd, id) {
   act_pd <- get_children(pd, id)
   act_sblngs <- act_pd[act_pd$parent == id, ]
   assigns_ids <- pd$parent[pd$token %in% assigns]
-  
+
   # remove function definitions
   act_pd <- remove_nodes(
     act_pd,
     act_pd$id[act_pd$parent == act_pd$parent[act_pd$token == "FUNCTION"]]
   )
-  
+
   lv_vars_ids <- c()
   # get for condition's IN vars
   # FOR '(' forcond ')' ; where forcond ~> SYMBOL IN expr
   lv_vars_ids <- c(lv_vars_ids, act_pd$id[which(act_pd$token == "IN") - 1])
-  
+
   # get updated vars, e.g., x <- x + 1
   lv_vars_ids <- c(lv_vars_ids, get_updated_vars_ids(act_pd))
-  
+
   old_lv_vars_ids <- c()
   while (length(lv_vars_ids) != length(old_lv_vars_ids)) {
     old_lv_vars_ids <- lv_vars_ids
@@ -202,7 +202,7 @@ get_loop_variant_vars <- function(pd, id) {
     }
     lv_vars_ids <- unique(lv_vars_ids)
   }
-  
+
   unique(act_pd$text[act_pd$id %in% lv_vars_ids])
 }
 
@@ -213,7 +213,7 @@ get_loop_variant_vars <- function(pd, id) {
 get_updated_vars_ids <- function(pd) {
   assigned_ids <- get_assigned_vars_ids(pd, get_roots(pd)$id)
   assigns_ids <- pd$parent[pd$token %in% assigns]
-  
+
   assigned_ids[sapply(assigned_ids, function(act_id) {
     ancestors_ids <- get_ancestors(pd, act_id)
     ass_prnt_id <- intersect(assigns_ids, ancestors_ids)
@@ -235,7 +235,7 @@ get_assigned_exprs_ids <- function(pd, id) {
   assign_exprs_prnts <- act_pd[
     act_pd$token %in% assigns & act_pd$text != ":=",
     "parent"
-    ]
+  ]
   # get the assigned expr pd id
   sapply(assign_exprs_prnts, function(assign_exprs_prnt) {
     aux <- act_pd[act_pd$parent == assign_exprs_prnt, ]
