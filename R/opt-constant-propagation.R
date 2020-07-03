f24d963d24d172291fcecddda3950b89c653d3e4
Rahul Saxena <rahulsaxena.pro@gmail.com>
2020-07-04 00:43:47 +0530

    Removed unnecessary changes

--- R/opt-constant-propagation.R
+++ R/opt-constant-propagation.R
@@ -16,15 +16,6 @@
 #'   sep = "\n"
 #' )
 #' cat(opt_constant_propagation(list(code))$codes[[1]])
-#' 
-#' hemisphere_vol <- paste(
-#'   " pi <- 3.141593 ",
-#'   " radius <- 25 ",
-#'   " hemis_vol <- 2/3 * pi * radius ^ 3 ",
-#'   " print(hemis_vol) ",
-#'   sep = "\n"
-#' )
-#' cat(opt_constant_propagation(list(hemisphere_vol))$codes[[1]])
 #' @export
 #'
 opt_constant_propagation <- function(texts, in_fun_call = FALSE) {
